c9173a89ffb6d71889646a3a189181ae0e0ba065
cjt243 <cjt243@att.net>
2018-08-31 19:21:20 -0400

    Adding file containing classes for games and users

---
+++ dartboard_classes.py
@@ -0,0 +1,11 @@
+class player(userid,username):
+    """docstring for player."""
+    def __init__(self, arg):
+        super(player, self).__init__()
+        self.arg = arg
+
+class game(object):
+    """docstring for game."""
+    def __init__(self, arg):
+        super(game, self).__init__()
+        self.arg = arg
