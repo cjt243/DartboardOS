10614bf923a9943b2a8b40f80eb444a2176a7fc1
cjt243 <cjt243@att.net>
2018-09-13 23:24:33 -0400

    App auto size to touchscreen, added static board elements.

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.uix.floatlayout import FloatLayout
+from kivy.config import Config
+Config.set('graphics', 'width', '480')
+Config.set('graphics', 'height', '800')
 
 class dartboardosGame(Widget):
     pass
@@ -7,6 +12,8 @@ class dartboardosGame(Widget):
 class dartboardosApp(App):
     def build(self):
         return dartboardosGame()
+
+
 
 if __name__ == '__main__':
     dartboardosApp().run()
