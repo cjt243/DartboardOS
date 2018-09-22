618a690c9e071458d780539cab9d132801d31003
cjt243 <cjt243@att.net>
2018-09-22 09:42:39 -0400

    Testing multiple screens

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.floatlayout import FloatLayout
 from kivy.config import Config
 Config.set('graphics', 'width', '480')
 Config.set('graphics', 'height', '800')
 
-class dartboardosGame(Widget):
+class dartboardosGame(Screen):
     pass
 
-class mainMenu(Widget):
+class mainMenu(Screen):
     pass
+
+sm = ScreenManager()
+sm.add_widget(dartboardosGame(name='scoreboard'))
+sm.add_widget(mainMenu(name='menu'))
 
 
 class dartboardosApp(App):
     def build(self):
-        return dartboardosGame()
-
-class mainMenuScreen(App):
-    def build(self):
-        return mainMenu()
+        return sm
 
 if __name__ == '__main__':
-    # dartboardosApp().run()
-    mainMenuScreen().run()
+    dartboardosApp().run()
+    # mainMenuScreen().run()
