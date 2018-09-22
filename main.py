3172426a5daca38a1a47726a12cf530e7ba3e922
cjt243 <cjt243@att.net>
2018-09-22 10:23:46 -0400

    Multi screen layout working.

--- main.py
+++ main.py
@@ -5,24 +5,26 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.floatlayout import FloatLayout
 from kivy.config import Config
-Config.set('graphics', 'width', '480')
-Config.set('graphics', 'height', '800')
+from kivy.core.window import Window
+# Config.set('graphics', 'width', '480')
+# Config.set('graphics', 'height', '800')
 
 class dartboardosGame(Screen):
+    def on_pre_enter(self):
+        Window.size = (480,800)
     pass
 
 class mainMenu(Screen):
+    def on_pre_enter(self):
+        Window.size = (480,800)
     pass
-
-sm = ScreenManager()
-sm.add_widget(dartboardosGame(name='scoreboard'))
-sm.add_widget(mainMenu(name='menu'))
-
 
 class dartboardosApp(App):
     def build(self):
+        sm = ScreenManager()
+        sm.add_widget(mainMenu(name='menu'))
+        sm.add_widget(dartboardosGame(name='scoreboard'))
         return sm
 
 if __name__ == '__main__':
     dartboardosApp().run()
-    # mainMenuScreen().run()
