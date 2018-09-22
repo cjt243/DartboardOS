3051b1a2455bd319792cf10c3c52d680170030c5
cjt243 <cjt243@att.net>
2018-09-22 12:18:35 -0400

    Cleaned up class names.

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
 from kivy.uix.floatlayout import FloatLayout
 from kivy.config import Config
 from kivy.core.window import Window
-# Config.set('graphics', 'width', '480')
-# Config.set('graphics', 'height', '800')
+Config.set('graphics', 'width', '480')
+Config.set('graphics', 'height', '800')
 
-class dartboardosGame(Screen):
+class Cricket(Screen):
     def on_pre_enter(self):
         Window.size = (480,800)
     pass
 
-class mainMenu(Screen):
+class Menu(Screen):
     def on_pre_enter(self):
         Window.size = (480,800)
     pass
@@ -22,8 +22,8 @@ class mainMenu(Screen):
 class dartboardosApp(App):
     def build(self):
         sm = ScreenManager()
-        sm.add_widget(mainMenu(name='menu'))
-        sm.add_widget(dartboardosGame(name='scoreboard'))
+        sm.add_widget(Menu(name='menu'))
+        sm.add_widget(Cricket(name='cricket'))
         return sm
 
 if __name__ == '__main__':
