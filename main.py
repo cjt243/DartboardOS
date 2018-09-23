b167d080f8f6ffad8fa26852ab87072a2467fdb9
cjt243 <cjt243@att.net>
2018-09-23 17:16:25 -0400

    Player selection working on Menu screen.

--- main.py
+++ main.py
@@ -10,28 +10,35 @@ from kivy.uix.dropdown import DropDown
 from database_functions import *
 
 class Cricket(Screen):
+    # def __init__(self, **kwargs):
+    #     super(Cricket, self).__init__(**kwargs)
+    #     self.ids.player1label.text = root.menu.player1select.text
+
     def on_pre_enter(self):
         Window.size = (480,800)
     pass
 
 class Menu(Screen):
-    # player_list_player = ListProperty([])
     def __init__(self, **kwargs):
         super(Menu, self).__init__(**kwargs)
         self.ids.player1select.text = 'Select Player 1'
+        self.ids.player2select.text = 'Select Player 2'
+        self.dropdown1 = DropDown()
+        self.dropdown2 = DropDown()
         self._get_players()
+        self._create_dropdowns(self.ids.player1select, self.dropdown1)
+        self._create_dropdowns(self.ids.player2select, self.dropdown2)
     def _get_players(self):
         sql = 'select username from player;'
         output = execute_select_statement(create_connection('gamedb/dartboardos.db'),sql)
         self.player_list_player = list(zip(*output))[0]
-    def _create_dropdowns(self,dropdown):
-        self.dropdown = DropDown()
+    def _create_dropdowns(self, button_id, dropdown):
         for i in self.player_list_player:
             btn = Button(text=i, size_hint_y=None, height=44)
             btn.bind(on_release=lambda btn: dropdown.select(btn.text))
             dropdown.add_widget(btn)
-        self.ids.player1select.bind(on_release=dropdown.open)
-        self.ids.player1select.dropdown.bind(on_select=lambda instance, x: setattr(self.ids.player1select, 'text', x))
+        button_id.bind(on_release=dropdown.open)
+        dropdown.bind(on_select=lambda instance, x: setattr(button_id, 'text', x))
     def on_pre_enter(self):
         Window.size = (480,800)
     pass
@@ -40,7 +47,6 @@ class dartboardosApp(App):
     def build(self):
         sm = ScreenManager()
         sm.add_widget(Menu(name='menu'))
-        # sm.add_widget(Menu())
         sm.add_widget(Cricket(name='cricket'))
         return sm
 
