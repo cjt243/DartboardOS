a72a09db0a0403d60a19389e329a747bc227b2d0
cjt243 <cjt243@att.net>
2018-10-27 16:33:42 -0400

    Winning game state, multiple games per session.

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
-from kivy.properties import ListProperty, NumericProperty, StringProperty
-from kivy.uix.floatlayout import FloatLayout
 from kivy.core.window import Window
 from kivy.uix.dropdown import DropDown
 import datetime
@@ -16,16 +14,16 @@ class Cricket(Screen):
     def on_pre_enter(self):
         Window.size = (480,800)
     def _set_active_players(self):
-        output_1 = execute_select_statement(create_connection('gamedb/dartboardos.db'),
+        output_1 = execute_sql_statement(create_connection('gamedb/dartboardos.db'),
                    "select player1_id,username from game_header left join player on game_header.player1_id = player.id where game_header.id = (select max(id) from game_header);")
-        output_2 = execute_select_statement(create_connection('gamedb/dartboardos.db'),
+        output_2 = execute_sql_statement(create_connection('gamedb/dartboardos.db'),
                    "select player2_id,username from game_header left join player on game_header.player2_id = player.id where game_header.id = (select max(id) from game_header);")
         self.player1id = output_1[0][0]
         self.ids.player1label.text = output_1[0][1]
         self.player2id = output_2[0][0]
         self.ids.player2label.text = output_2[0][1]
     def _setup_game(self):
-        output_1 = execute_select_statement(create_connection('gamedb/dartboardos.db'),
+        output_1 = execute_sql_statement(create_connection('gamedb/dartboardos.db'),
                    "select max(id) from game_header;")
         self.gameid = output_1[0][0]
         self.game_end = ''
@@ -33,6 +31,32 @@ class Cricket(Screen):
         self.player2_scoreboard = {'20':0,'19':0,'18':0,'17':0,'16':0,'15':0,'25':0}
         self.player1_score = 0
         self.player2_score = 0
+    def _clear_game(self):
+        self.gameid = 0
+        self.game_end = ''
+        self.player1_scoreboard = {'20':0,'19':0,'18':0,'17':0,'16':0,'15':0,'25':0}
+        self.player2_scoreboard = {'20':0,'19':0,'18':0,'17':0,'16':0,'15':0,'25':0}
+        self.player1_score = 0
+        self.player2_score = 0
+        self.ids.p120.text = '0'
+        self.ids.p119.text = '0'
+        self.ids.p118.text = '0'
+        self.ids.p117.text = '0'
+        self.ids.p116.text = '0'
+        self.ids.p115.text = '0'
+        self.ids.p125.text = '0'
+        self.ids.p220.text = '0'
+        self.ids.p219.text = '0'
+        self.ids.p218.text = '0'
+        self.ids.p217.text = '0'
+        self.ids.p216.text = '0'
+        self.ids.p215.text = '0'
+        self.ids.p225.text = '0'
+        self.ids.player1score.text = '0'
+        self.ids.player2score.text = '0'
+        self.ids.player1winner.text = ''
+        self.ids.player2winner.text = ''
+
     def mark_hit(self, player_num,dart_val):
         dart_val = str(dart_val)
         if player_num == 1:
@@ -49,6 +73,13 @@ class Cricket(Screen):
                 else:
                     point_val = 0
             self.ids.player1score.text = str(self.player1_score)
+            if self._check_if_winner(1):
+                update = [datetime.datetime.now(),self.player1id,self.player1_score,self.player2_score,self.gameid]
+                update_game_header(create_connection('gamedb/dartboardos.db'),update)
+                self.ids.player1winner.text = 'Winner!'
+            else:
+                pass
+
         elif player_num == 2:
             if self.player2_scoreboard[dart_val] < 3:
                 self.player2_scoreboard[dart_val] += 1
@@ -63,6 +94,12 @@ class Cricket(Screen):
                 else:
                     point_val = 0
             self.ids.player2score.text = str(self.player2_score)
+            if self._check_if_winner(2):
+                update = [datetime.datetime.now(),self.player2id,self.player1_score,self.player2_score,self.gameid]
+                update_game_header(create_connection('gamedb/dartboardos.db'),update)
+                self.ids.player2winner.text = 'Winner!'
+            else:
+                pass
         else:
             print('Passed a player value != 1 or 2. Killing app.')
             exit()
@@ -125,6 +162,32 @@ class Cricket(Screen):
                      playerid]
         create_game_line(create_connection('gamedb/dartboardos.db'),game_line)
 
+    def _check_if_winner(self,player_num):
+        checker = 0
+        if player_num == 1:
+            for v in list(self.player1_scoreboard.values()):
+                if v < 3:
+                    return False
+                else:
+                    pass
+            if self.player1_score > self.player2_score:
+                return True
+            else:
+                return False
+        elif player_num == 2:
+            for v in list(self.player2_scoreboard.values()):
+                if v < 3:
+                    return False
+                else:
+                    pass
+            if self.player2_score > self.player1_score:
+                return True
+            else:
+                return False
+        else:
+            exit()
+
+
 class Menu(Screen):
     def __init__(self, **kwargs):
         super(Menu, self).__init__(**kwargs)
@@ -137,7 +200,7 @@ class Menu(Screen):
         self._create_dropdowns(self.ids.player2select, self.dropdown2)
     def _get_players(self):
         sql = 'select username from player;'
-        output = execute_select_statement(create_connection('gamedb/dartboardos.db'),sql)
+        output = execute_sql_statement(create_connection('gamedb/dartboardos.db'),sql)
         self.player_list_player = list(zip(*output))[0]
     def _create_dropdowns(self, button_id, dropdown):
         for i in self.player_list_player:
@@ -152,9 +215,9 @@ class Menu(Screen):
         header = ['cricket',
                   datetime.datetime.now(),
                   '',
-                  execute_select_statement(create_connection('gamedb/dartboardos.db'),
+                  execute_sql_statement(create_connection('gamedb/dartboardos.db'),
                   "select id from player where username = '{}';".format(self.ids.player1select.text))[0][0],
-                  execute_select_statement(create_connection('gamedb/dartboardos.db'),
+                  execute_sql_statement(create_connection('gamedb/dartboardos.db'),
                   "select id from player where username = '{}';".format(self.ids.player2select.text))[0][0],
                   0,
                   0,
